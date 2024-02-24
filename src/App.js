0867e312ca6779dc259dca5070ac7616d74bf9e4
rakeshkhaarol <kharol64666@gmail.com>
2024-02-24 16:08:52 +0530

    c1

---
+++ src/App.js
@@ -0,0 +1,22 @@
+//1. import area
+import React from 'react'
+
+
+//2. definetion area
+
+function App() {
+//2.1 hooks area
+
+
+//2.2 function definetion area
+
+
+
+//2.3 return statments
+  return (
+    <h1>hello React </h1>
+  )
+}
+
+//3. export area
+export default App
